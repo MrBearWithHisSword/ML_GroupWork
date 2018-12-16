8f7f4e78bb1b984bb8dffde47b4f6e03aac2404a
MrBearWithHisSword <735683079@qq.com>
2018-12-16 16:13:33 +0800

    Update hyperparameters.

--- face_verification/hyperparams.py
+++ face_verification/hyperparams.py
@@ -6,8 +6,10 @@ class Hyperparameters():
     train_filename = '/home/hcshi/Class_Assignment/01_FaceVerification/code/inception_res_v2/fake_data/face_train'
     validation_filename = '/home/hcshi/Class_Assignment/01_FaceVerification/code/inception_res_v2/fake_data/face_validation'
 
+
     split_train_rate = 0.8
     img_height = 299
     img_width = 299
     train_pair_num = 0
     validation_pair_num = 0
+    max_anchor = 10
